dfe920f70ef08b8f314ee1d55925fc656a803563
theobcl <tbancal@free.fr>
2023-02-03 19:38:24 +0100

    initiate homepage cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,18 @@
-function Card() {
+import styled from 'styled-components';
+
+function Card({ id, image, titre }) {
   return (
-    <div>
-      <h1>Card</h1>
-    </div>
+    <CardStyled id={id} image={image}>
+      <span>{titre}</span>
+    </CardStyled>
   );
 }
+
+const CardStyled = styled.div`
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url(${(props) => props.image});
+  height: 21rem;
+  width: 21rem;
+`;
 
 export default Card;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,37 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
 import Banner from '../components/Banner';
 import BannerImg from '../assets/Banner/home-banner.jpg';
+import LodgingList from '../data/logement.json';
+import Card from '../components/Card';
 
 function Home() {
-  return <Banner image={BannerImg} text="Chez vous, partout et ailleurs" />;
+  return (
+    <>
+      <Banner image={BannerImg} text="Chez vous, partout et ailleurs" />
+      <CardsContainer>
+        {LodgingList.map((lodging) => (
+          <NavLink key={lodging.id} to="/">
+            <Card
+              key={lodging.id}
+              id={lodging.id}
+              image={lodging.cover}
+              titre={lodging.title}
+            />
+          </NavLink>
+        ))}
+      </CardsContainer>
+    </>
+  );
 }
+
+const CardsContainer = styled.div`
+  background-color: #f7f7f7;
+  border-radius: 1.5rem;
+  padding: 3rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
 
 export default Home;

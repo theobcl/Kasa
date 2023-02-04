aa13db92f63f8472835373f18572ad6095568af9
theobcl <tbancal@free.fr>
2023-02-04 15:36:48 +0100

    Improve about list

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,27 +2,15 @@ import React from 'react';
 import Banner from '../components/Banner';
 import aboutBanner from '../assets/About/about-banner.jpg';
 import Dropdown from '../components/Dropdown';
+import AboutList from '../data/about.json';
 
 function About() {
   return (
     <div>
       <Banner image={aboutBanner} text="" />
-      <Dropdown
-        title="Fiabilité"
-        description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
-      />
-      <Dropdown
-        title="Respect"
-        description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-      />
-      <Dropdown
-        title="Service"
-        description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
-      />
-      <Dropdown
-        title="Sécurité"
-        description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-      />
+      {AboutList.map((about) => (
+        <Dropdown title={about.title} description={about.description} />
+      ))}
     </div>
   );
 }
